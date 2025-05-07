c701ec73a2a25111d35d15147e2ffb37cb8db24c
MagaJL <48716048@est.ort.edu.ar>
2025-05-07 18:52:29 -0300

    Ejercicio VTV terminado

---
+++ Assets/Scripts/VTV.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VTV : MonoBehaviour
+{
+    public int patente;
+    public int modelo;
+    public int kilometrosRecorridos;
+    public int vencimientoVTV;
+    public int HC;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (patente == 0 || modelo < 1900 || modelo > 2025 || kilometrosRecorridos < 0 || vencimientoVTV > 2025 || vencimientoVTV < modelo || HC < 5 || HC > 100)
+        {
+            Debug.Log("Alguno de los datos no es válido. " +
+                "VTV no aprobada.");
+            return;
+        }
+        else if (kilometrosRecorridos < 10000)
+        {
+            Debug.Log("VTV otorgada por 2 años.");
+        }
+        else
+        {
+            Debug.Log("VTV otorgada por 1 año.");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
