46f78346d8ac1db05631a4e4de8ec2322c97f59e
MagaJL <48716048@est.ort.edu.ar>
2025-05-07 20:40:19 -0300

    Ejercicio Flota de Taxis terminado

---
+++ Assets/Scripts/FlotaDeTaxis.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlotaDeTaxis : MonoBehaviour
+{
+    public int cantidadDeUnidades;
+    public float periodoDeDias;
+    float costoDeLitroXUnidadXDia = 780;
+    float costoTotalCombustible;
+    float costoTotalConDescuento;
+    float descuento = 0.2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (periodoDeDias < 5 || cantidadDeUnidades < 1)
+        {
+            Debug.Log("Alguno de los datos ingresados es invalido.");
+            return;
+        }
+        else 
+        {
+            costoTotalCombustible = costoDeLitroXUnidadXDia * cantidadDeUnidades * periodoDeDias;
+
+            if (costoTotalCombustible > 130000)
+            {
+                costoTotalCombustible = costoTotalCombustible - costoTotalCombustible * descuento;
+            }
+            else
+            {
+                costoTotalCombustible = costoTotalCombustible;
+            }
+
+            Debug.Log("Una flota de " + cantidadDeUnidades + " unidades trabajando durante " + 
+                periodoDeDias + " días implicará un gasto de $" + costoTotalCombustible + " pesos en concepto de combustible.");
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
